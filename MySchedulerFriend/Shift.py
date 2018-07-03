c1cff51e7c4b25808df5506cdb99233d056e188e
Pavlo Triantafyllides <SATAN@Pavlos-MBP-2.attlocal.net>
2018-07-03 18:20:09 -0400

    Added test json

---
+++ MySchedulerFriend/Shift.py
@@ -0,0 +1,6 @@
+import datetime
+
+class Shift():
+    def __init__(self, StartTime, EndTime):
+        self.StartTime = StartTime
+        self.EndTime = EndTime
--- MySchedulerFriend/Worker.py
+++ MySchedulerFriend/Worker.py
@@ -4,5 +4,3 @@ class Worker():
     def __init__(self, Name, AvailableDayTimeList):
         self.Name = Name
         self.AvailableDayTimeList = AvailableDayTimeList
-
-
