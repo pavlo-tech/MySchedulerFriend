6f5f2fc3dc01f5d6325873f09588f46bb0f5d3a6
silkytiger <triantafyllides@gmail.com>
2018-07-03 16:57:36 -0400

    Add project files.

---
+++ MySchedulerFriend/MySchedulerFriend.py
@@ -0,0 +1,24 @@
+import sys
+import calendar
+import datetime
+
+import Worker
+
+'''
+Get start date
+'''
+C
+
+'''
+Init all workers
+'''
+Workers 
+
+'''
+fill calendar
+'''
+
+
+
+
+
---
+++ MySchedulerFriend/Worker.py
@@ -0,0 +1,8 @@
+
+
+class Worker():
+    def __init__(self, Name, AvailableDayTimeList):
+        self.Name = Name
+        self.AvailableDayTimeList = AvailableDayTimeList
+
+
